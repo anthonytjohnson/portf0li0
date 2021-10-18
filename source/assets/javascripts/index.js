04a7f47f3a3c5ed2d6bed17d8f5fdb7eca4938bc
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-18 20:37:18 +0900

    added css

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,24 @@
 import 'bootstrap';
+document.ready(function(){
+
+	$("nav a").on("click", function(event){
+		event.preventDefault();
+		$("nav").classList.add("fixed");
+		id = (this.attr("href"));
+		scrollVertical = id.offset().top;
+
+		$("body, html").animate({scrollTop: scrollVertical});
+	});
+
+	document.on("scroll", function(){
+		secondPage = $("nav li:nth-child(2) a").attr("href");
+
+		if ( $("body").scrollTop() >= $("nav").height() )
+		{
+			$("nav").classList.add("fixed");
+		} else {
+			$("nav").classList.remove("fixed");
+		}
+	});
+
+});
